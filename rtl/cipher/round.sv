db78378f467c0608689779fb9498afb9634b7ae9
omarmuhammadmu <omar.AbdeLatif99@eng-st.cu.edu.eg>
2024-10-05 15:23:57 +0300

    files rearranging and dechiper initial

--- rtl/cipher/round.sv
+++ rtl/cipher/round.sv
@@ -1,3 +1,35 @@
+/* -----------------------------------------------------------------------------
+   Copyright (c) Omar Muhammad Mustafa
+   -----------------------------------------------------------------------------
+   FILE NAME : round.sv
+   DEPARTMENT : aes enrypthion
+   AUTHOR : Omar Muhammad
+   AUTHOR’S EMAIL : omarmuhammadmu0@gmail.com
+   -----------------------------------------------------------------------------
+   RELEASE HISTORY
+   VERSION  DATE        AUTHOR      DESCRIPTION
+   1.0      2024-10-5               initial version
+   -----------------------------------------------------------------------------
+   KEYWORDS : AES
+   -----------------------------------------------------------------------------
+   PURPOSE : implementation of encryption round
+   -----------------------------------------------------------------------------
+   PARAMETERS
+   PARAM NAME   : RANGE   : DESCRIPTION         : DEFAULT   : UNITS
+   N/A          : N/A     : N/A                 : N/A       : N/A 
+   -----------------------------------------------------------------------------
+   REUSE ISSUES
+   Reset Strategy   : 
+   Clock Domains    : 
+   Critical Timing  : 
+   Test Features    : 
+   Asynchronous I/F : 
+   Scan Methodology : 
+   Instantiations   : 
+   Synthesizable    : Y
+   Other            : 
+   -FHDR------------------------------------------------------------------------*/
+
 import aes_package::*;
 
 module round #(
